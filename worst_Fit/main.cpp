0c146bbc6b79c588f5c5b62da633eac9b6bebc01
Jon Lowrey <maclowrey@gmail.com>
2018-04-19 18:03:45 -0500

    Initial Commit

---
+++ worst_Fit/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  worst_Fit
+//
+//  Created by Jon Lowrey on 4/19/18.
+//  Copyright © 2018 Jon Lowrey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
